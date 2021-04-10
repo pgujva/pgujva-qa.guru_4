db04644d3c4e77e4d29af949cba44f110bf78291
pgujva <pgujva@usetech.ru>
2021-04-10 18:57:16 +0300

    pageobject

--- src/test/java/FillFormTest.java
+++ src/test/java/FillFormTest.java
@@ -1,8 +1,4 @@
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
 
 import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
@@ -15,33 +11,20 @@ public class FillFormTest extends TestBase{
   @Test
   void successfulFillTest() {
     goToPracticeForm();
-    $("#firstName").setValue("pasha");
-    $("#lastName").setValue("gujva");
-    $("#userEmail").setValue("test@mail.ru");
-    $("#genterWrapper").find(byText("Male")).click();
-    $("#userNumber").setValue("8904442222");
-    //календарь
-    $("#dateOfBirthInput").clear();
-    $(".react-datepicker__month-select").selectOption("July");
-    $(".react-datepicker__year-select").selectOption("1991");
-    $(".react-datepicker__day.react-datepicker__day--017").click();
-    // set subject
-    $("#subjectsContainer input").setValue("te").pressEnter();
-    $(byText("Reading")).click();
-    // загрузка файла
-    $("#uploadPicture").uploadFromClasspath("test1.jpg");
-    $("#currentAddress").setValue("5555");
-    $("#state").click();
-    $(byText("Uttar Pradesh")).click();
-    $("#city").click();
-    $(byText("Lucknow")).click();
+    fillTestForm(new PracticeFormData("pasha",
+            "gujva",
+            "test@mail.ru",
+            "Male",
+            "8904442222",
+            "July",
+            "1991",
+            "English",
+            "Reading",
+            "5555",
+            "Uttar Pradesh",
+            "Lucknow"));
     submitForm();
-    $$(".table-responsive tr" ).filterBy(text("Student name")).shouldHave(texts("pasha " + "gujva"));
-    $$(".table-responsive tr" ).filterBy(text("Student Email")).shouldHave(texts("test@mail.ru"));
-  }
-
-  private void submitForm() {
-    $("#submit").click();
+    asserts();
   }
 
 }
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
 
@@ -13,5 +16,37 @@ public class TestBase {
 
   public static void goToPracticeForm() {
     open("https://demoqa.com/automation-practice-form");
+  }
+
+  protected void asserts() {
+    $$(".table-responsive tr" ).filterBy(text("Student name")).shouldHave(texts("pasha " + "gujva"));
+    $$(".table-responsive tr" ).filterBy(text("Student Email")).shouldHave(texts("test@mail.ru"));
+  }
+
+  protected void fillTestForm(PracticeFormData practiceFormData) {
+    $("#firstName").setValue(practiceFormData.getName());
+    $("#lastName").setValue(practiceFormData.getLastname());
+    $("#userEmail").setValue(practiceFormData.getEmail());
+    $("#genterWrapper").find(byText(practiceFormData.getGender())).click();
+    $("#userNumber").setValue(practiceFormData.getPhone());
+    //календарь
+    $("#dateOfBirthInput").clear();
+    $(".react-datepicker__month-select").selectOption(practiceFormData.getMonth());
+    $(".react-datepicker__year-select").selectOption(practiceFormData.getYear());
+    $(".react-datepicker__day.react-datepicker__day--017").click();
+    // set subject
+    $("#subjectsContainer input").setValue(practiceFormData.getSubject()).pressEnter();
+    $(byText(practiceFormData.getHobby())).click();
+    // загрузка файла
+    $("#uploadPicture").uploadFromClasspath("test1.jpg");
+    $("#currentAddress").setValue(practiceFormData.getAddress());
+    $("#state").click();
+    $(byText(practiceFormData.getState())).click();
+    $("#city").click();
+    $(byText(practiceFormData.getCity())).click();
+  }
+
+  protected void submitForm() {
+    $("#submit").click();
   }
 }
