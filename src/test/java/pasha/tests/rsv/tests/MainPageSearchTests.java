1dbcd53774706cd82e91b20d357746d4ea8673c5
pgujva <pgujva@usetech.ru>
2021-04-12 00:05:43 +0300

    refactor

--- src/test/java/pasha/tests/rsv/tests/MainPageSearchTests.java
+++ src/test/java/pasha/tests/rsv/tests/MainPageSearchTests.java
@@ -1,23 +1,26 @@
-package pasha.test.fillformtests;
+package pasha.tests.rsv.tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.conditions.Text;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class Rsvtest {
-  @Test
-  public void testsearch() {
-    String webinar = "Вебинар";
+public class MainPageSearchTests {
+  @BeforeAll
+  static void setUp() {
     SelenideLogger.addListener("allure", new AllureSelenide());
     Configuration.startMaximized = true;
     open("https://rsv-test.bizml.ru/");
+  }
+
+  @Test
+  public void testsearch() {
+    String webinar = "Вебинар";
     $(".search-button").click();
     $(".searching-input").clear();
     $(".searching-input").val(webinar);
